b3140eda2769b1a39e2070d76b812651a6c63abb
Albert-Asadov <albertma@code.edu.az>
2023-05-02 21:23:38 +0400

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var login = document.querySelector(".login");
+var panel = document.querySelector(".panel");
+var empty = document.querySelector(".empty")
+
+login.addEventListener("click", function(){
+  
+    empty.style.display = "block"
+})
