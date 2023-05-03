2dce27a62e68ae901177005e30e5a7fa6cf2bf88
Albert-Asadov <albertma@code.edu.az>
2023-05-03 12:45:06 +0400

    Headers, Slider, Reklam, About Pasge

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,22 @@ login.addEventListener("click", function(){
   
     empty.style.display = "block"
 })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    // margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
