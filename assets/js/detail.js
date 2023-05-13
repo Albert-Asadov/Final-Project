3aa2724f5c97ebe5be411723df2b1357bd6dfdd6
Albert-Asadov <albertma@code.edu.az>
2023-05-13 19:59:30 +0400

    detail de duzelishler ve elaveler

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -36,6 +36,46 @@ $('.responsive').slick({
     ]
   });
 
+
+
+  $('.products').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<i class="fa-solid fa-angle-right next_arrow"></i>',
+    prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
   var WishlistPanel = document.querySelector(".panelWish");
   var wishlistButton = document.querySelector(".wishlist");
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -146,22 +146,79 @@ $('.responsive').slick({
   });
 
 
-$('.products').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000,
-  dots:false,
-  nextArrow: '<i class="fa-solid fa-angle-right next_arrow"></i>',
-  prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
-});
+  $('.products').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<i class="fa-solid fa-angle-right next_arrow"></i>',
+    prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-$('.allForMd').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000,
-  dots:false,
-  nextArrow: '<i class="fa-solid fa-angle-right next_arrow"></i>',
-  prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
-});
+
+  $('.allForMd').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<i class="fa-solid fa-angle-right next_arrow"></i>',
+    prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
